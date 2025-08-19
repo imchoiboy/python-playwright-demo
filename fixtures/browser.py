d47c25610e942bea9c6c77962f47ddcf2aa59e68
matthewchoi66 <matthewchoi66@gmail.com>
2025-08-19 12:00:00 -0400

    commenting out unnecessary fixture + additions to pytest.ini + removing unnecessary requirements

--- fixtures/browser.py
+++ fixtures/browser.py
@@ -1,18 +1,18 @@
-import pytest
-from playwright.sync_api import sync_playwright
-from utils.config import Config
-
-@pytest.fixture(scope="session")
-def playwright_context(browser):
-    with sync_playwright() as p:
-        browser = getattr(p, Config.BROWSER).launch(headless=Config.HEADLESS)
-        context = browser.new_context(base_url=Config.BASE_URL)
-        yield context
-        context.close()
-        browser.close()
-
-@pytest.fixture
-def page(playwright_context):
-    page = playwright_context.new_page()
-    yield page
-    page.close()
+# import pytest
+# from playwright.sync_api import sync_playwright
+# from utils.config import Config
+#
+# @pytest.fixture(scope="session")
+# def playwright_context(browser):
+#     with sync_playwright() as p:
+#         browser = getattr(p, Config.BROWSER).launch(headless=Config.HEADLESS)
+#         context = browser.new_context(base_url=Config.BASE_URL)
+#         yield context
+#         context.close()
+#         browser.close()
+#
+# @pytest.fixture
+# def page(playwright_context):
+#     page = playwright_context.new_page()
+#     yield page
+#     page.close()
